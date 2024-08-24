8f45b30a5ae74337626c60c170fe96def20cb250
hum-dev <humphreymuriungi@outlook.com>
2024-08-24 07:30:23 +0300

    Added a spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+
+
+export default function loading() {
+  return (
+      <div className="flex justify-center items-center h-screen"> 
+          <img src="spinner.svg" alt="Loading..." />
+      </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Results from "@/components/Results";
+import { resolve } from "styled-jsx/css";
 
 const API = process.env.API_KEY;
 export default async function Home({searchParams}) {
@@ -14,15 +15,14 @@ export default async function Home({searchParams}) {
     // "fetchSciFiMovies" ||
     // "fetchWesternMovies" ||
     // "fetchAnimationMovies" ||
-    // "fetchTV";
+  // "fetchTV";
   const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API}&language=en-US&page=1`
+      `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
   );
-
   const data = await res.json();
-  if (!res) {
+  if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
   const results = data.results;
