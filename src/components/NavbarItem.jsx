76f055928dc5ae649ec7b444626c05fc0fe322dc
hum-dev <humphreymuriungi@outlook.com>
2024-08-25 19:19:04 +0300

    fix the suspense bug

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 
 export default function NavbarItem({ title, param }) {
@@ -8,8 +9,10 @@ export default function NavbarItem({ title, param }) {
     const genre = searchParams.get('genre');
   return (
       <div >
-          <Link className={`hover:text-blue-500 font-semibold ${genre === param ? 'underline underline-offset-8 decortion-4 decoration-blue-600 rounded-lg' : ''}`}
-              href={`/?genre=${param}`}>{title}</Link>
+          <Suspense>
+              <Link className={`hover:text-blue-500 font-semibold ${genre === param ? 'underline underline-offset-8 decortion-4 decoration-blue-600 rounded-lg' : ''}`}
+                  href={`/?genre=${param}`}>{title}</Link>
+          </Suspense>
       </div>
   )
 }
