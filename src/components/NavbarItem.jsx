84ecf2a8dc14ea57d3cf452e14842d28558c6707
hum-dev <humphreymuriungi@outlook.com>
2024-08-28 05:27:39 +0300

    fixed the search params

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,39 +1,30 @@
-
-// export default function NavbarItem({ title, param }) {
-//     const searchParams = useSearchParams();
-//     const genre = searchParams.get('genre');
-//   return (
-//       <div >
-          
-//               <Link className={`hover:text-blue-500 font-semibold ${genre === param ? 'underline underline-offset-8 decortion-4 decoration-blue-600 rounded-lg' : ''}`}
-//                   href={`/?genre=${param}`}>{title}</Link>
-          
-//       </div>
-//   )
-// }
 'use client'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-// Function to get search parameters
-
-function NavbarItem({ title, param }) {
-    const searchParams = searchParams();
+const NavbarItem = ({ title, param }) => {
+    const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
+    
     return (
         <div>
-            <Link className={`hover:text-blue-500 font-semibold ${genre === param ? 'underline underline-offset-8 decortion-4 decoration-blue-600 rounded-lg' : ''}`}
-                href={`/?genre=${param}`}>{title}</Link>
+            <Link 
+                className={`hover:text-blue-500 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-blue-600 rounded-lg' : ''}`}
+                href={`/?genre=${param}`}
+            >
+                {title}
+            </Link>
         </div>
-    )
+    );
 }
 
-// Wrap NavbarItem with Suspense
-export default function SuspendedNavbarItem(props) {
+const SuspendedNavbarItem = (props) => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <NavbarItem {...props} />
         </Suspense>
     );
 }
+
+export default SuspendedNavbarItem;
